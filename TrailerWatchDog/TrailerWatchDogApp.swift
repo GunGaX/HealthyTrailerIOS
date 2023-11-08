c7eb86e449ad3e1e38559d6e2ced03e54ae401e1
GunGaX <dmytro.fwork@gmail.com>
2023-11-08 16:42:54 +0200

    Initial Commit

---
+++ TrailerWatchDog/TrailerWatchDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailerWatchDogApp.swift
+//  TrailerWatchDog
+//
+//  Created by Dmytro Savka on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrailerWatchDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
