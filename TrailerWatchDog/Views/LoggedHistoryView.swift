93bddcbeca6f79376c06c47e37ce3280ab5eb3dc
GunGaX <dmytro.fwork@gmail.com>
2023-11-10 15:05:50 +0200

    add: headers logic, navigation

---
+++ TrailerWatchDog/Views/LoggedHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  LoggedHistoryView.swift
+//  TrailerWatchDog
+//
+//  Created by Dmytro Savka on 10.11.2023.
+//
+
+import SwiftUI
+
+struct LoggedHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoggedHistoryView()
+}
---
+++ TrailerWatchDog/Views/TerminalView.swift
@@ -0,0 +1,18 @@
+//
+//  TerminalView.swift
+//  TrailerWatchDog
+//
+//  Created by Dmytro Savka on 10.11.2023.
+//
+
+import SwiftUI
+
+struct TerminalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TerminalView()
+}
