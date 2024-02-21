338d7b2b2076ae9efa8f265b168036b83d595450
GunGaX <dmytro.fwork@gmail.com>
2024-02-21 10:28:12 +0200

    add: parsing logic, new twd model

--- TrailerWatchDog/Managers/BluetoothTWDManager.swift
+++ TrailerWatchDog/Managers/BluetoothTWDManager.swift
@@ -13,11 +13,12 @@ class BluetoothTWDManager: NSObject, ObservableObject, CBCentralManagerDelegate,
     
     private let temperature_characteristic = CBUUID(string: "FFE1")
     
-    private var characteristicData: [CBCharacteristic] = []
+    private var centralManager: CBCentralManager!
     
     private var peripheral: CBPeripheral?
-
-    private var centralManager: CBCentralManager!
+    private var characteristicData: [CBCharacteristic] = []
+    
+    var fetchedTemperatureLine: String = ""
     
     @Published var discoveredPeripherals: [CBPeripheral] = []
     
@@ -85,10 +86,64 @@ class BluetoothTWDManager: NSObject, ObservableObject, CBCentralManagerDelegate,
         
         if characteristic.uuid == temperature_characteristic {
             if let value = characteristic.value, let stringValue = String(data: value, encoding: .utf8) {
-                print(stringValue)
+                fetchedTemperatureLine += stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                if fetchedTemperatureLine.hasSuffix("</R>") {
+                    let newTWD = parseData(dataString: fetchedTemperatureLine)
+//                    print(fetchedTemperatureLine)
+                    fetchedTemperatureLine = ""
+                    
+                    print(newTWD ?? "no data")
+                }
             } else {
                 print("Received empty or invalid value for characteristic \(characteristic.uuid)")
             }
         }
     }
+    
+    func parseData(dataString: String) -> NewTWDModel? {
+        guard let peripheral else { return nil }
+        
+        let components = dataString.components(separatedBy: "</L>,<R>")
+        guard components.count == 2 else { return nil }
+        
+        let leftAxleComponents = components[0].components(separatedBy: ",")
+        let rightAxleComponents = components[1].components(separatedBy: ",")
+        
+        var newTWDModel = NewTWDModel(id: peripheral.identifier , leftAxle: Axle(), rightAxle: Axle())
+        
+        func getTemperatureValue(from component: String) -> Double? {
+            let components = component.components(separatedBy: ":")
+                        
+            guard components.count == 2, let temperature = Double(components[1].replacingOccurrences(of: " F>", with: "").replacingOccurrences(of: "F></R>", with: "")) else { return nil }
+            
+            return temperature
+        }
+                
+        newTWDModel.leftAxle.firstTire = getTemperatureValue(from: leftAxleComponents[1])
+        newTWDModel.leftAxle.secondTire = getTemperatureValue(from: leftAxleComponents[2])
+        newTWDModel.leftAxle.thirdTire = getTemperatureValue(from: leftAxleComponents[3])
+        newTWDModel.leftAxle.fourthTire = getTemperatureValue(from: leftAxleComponents[4])
+        
+        newTWDModel.rightAxle.firstTire = getTemperatureValue(from: rightAxleComponents[1])
+        newTWDModel.rightAxle.secondTire = getTemperatureValue(from: rightAxleComponents[2])
+        newTWDModel.rightAxle.thirdTire = getTemperatureValue(from: rightAxleComponents[3])
+        newTWDModel.rightAxle.fourthTire = getTemperatureValue(from: rightAxleComponents[4])
+                
+        return newTWDModel
+    }
 }
+
+struct NewTWDModel {
+    let id: UUID
+    var leftAxle: Axle
+    var rightAxle: Axle
+}
+
+struct Axle {
+    var firstTire: Double?
+    var secondTire: Double?
+    var thirdTire: Double?
+    var fourthTire: Double?
+}
+
