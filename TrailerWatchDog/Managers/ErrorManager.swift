86934820b2330b1f8902cda07ee21b3d2aa07d46
GunGaX <dmytro.fwork@gmail.com>
2024-02-27 14:05:09 +0200

    add: check twd overheat logic

--- TrailerWatchDog/Managers/ErrorManager.swift
+++ TrailerWatchDog/Managers/ErrorManager.swift
@@ -12,21 +12,53 @@ final class ErrorManager: ObservableObject {
     var settingsViewModel = SettingsViewModel.shared
     var twdManager = BluetoothTWDManager.shared
     
-//    @Published var temperatureOverheatTPMSError = false
-//    var temperatureOverheatTPMSMessage = ""
-//    var temperatureOverheatTPMSMessageChanged = false
+    @Published var temperatureOverheatTWDError = false
+    var temperatureOverheatTWDMessage = ""
+    var temperatureOverheatTWDMessageChanged = false
     
     private var cancellable: AnyCancellable?
     
     init() {
         cancellable = twdManager.$connectedTWD.sink { twd in
-            print(twd?.leftAxle[0].last ?? "no info")
+            var hasOverheat = self.checkOverheatTWD(twd: twd)
+            self.setTemperatureOverheatTWD(isOverheat: hasOverheat)
         }
     }
     
-//    private func getMaxAllowedTemperatureTWD() -> Double {
-//        return settingsViewModel.maxTWDSensorTemperature
-//    }
+    private func getMaxAllowedTemperatureTWD() -> Double {
+        return settingsViewModel.maxTWDSensorTemperature
+    }
     
-//    private func checkOverheatTWD(data: )
+    private func checkOverheatTWD(twd: TWDModel?) -> Bool {
+        guard let twd else { return false }
+        
+        let maxAllowedTemperature = getMaxAllowedTemperatureTWD()
+        var messageBuilder = ""
+        var hasOverheat = false
+        
+        for index in 0..<twd.leftAxle.count {
+            if twd.leftAxle[index].last ?? 0.0 > maxAllowedTemperature {
+                messageBuilder += "Left TWD sensor \(index + 1) is over temperature threshold\n"
+                hasOverheat = true
+            }
+            
+            if twd.rightAxle[index].last ?? 0.0 > maxAllowedTemperature {
+                messageBuilder += "Right TWD sensor \(index + 1) is over temperature threshold\n"
+                hasOverheat = true
+            }
+        }
+        
+        temperatureOverheatTWDMessage = messageBuilder
+        
+        return hasOverheat
+    }
+    
+    private func setTemperatureOverheatTWD(isOverheat: Bool) {
+        if temperatureOverheatTWDError != isOverheat || temperatureOverheatTWDMessageChanged {
+            temperatureOverheatTWDMessageChanged = false
+            temperatureOverheatTWDError = isOverheat
+            
+            print(temperatureOverheatTWDMessage)
+        }
+    }
 }
