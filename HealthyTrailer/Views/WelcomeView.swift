2d11b4691b267486140db03b104c65c4e6a6f9ce
GunGaX <dmytro.fwork@gmail.com>
2024-05-25 20:12:51 +0300

    start localization

--- HealthyTrailer/Views/WelcomeView.swift
+++ HealthyTrailer/Views/WelcomeView.swift
@@ -32,14 +32,14 @@ struct WelcomeView: View {
     }
     
     private var title: some View {
-        Text("Welcome to Healthy Trailer App")
+        Text(.init("Welcome to Healthy Trailer App"))
             .font(.roboto400, size: 22)
             .foregroundStyle(Color.mainBlue)
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
     private var descriptionText: some View {
-        Text("This is a security and safety for your trailer in one click. To use this application, you must grant the following permissions:")
+        Text(.init("This is a security and safety for your trailer in one click. To use this application, you must grant the following permissions:"))
             .fontWeight(.regular)
             .multilineTextAlignment(.leading)
             .foregroundStyle(Color.textDark)
@@ -47,7 +47,7 @@ struct WelcomeView: View {
     }
     
     private var permissions: some View {
-        Text("• Access to your device's location \n• Bluetooth activation")
+        Text(.init("• Access to your device's location \n• Bluetooth activation"))
             .frame(maxWidth: .infinity, alignment: .leading)
             .fontWeight(.regular)
             .multilineTextAlignment(.leading)
@@ -56,7 +56,7 @@ struct WelcomeView: View {
     }
     
     private var conclusionText: some View {
-        Text("These permissions are essential for the app to function effectively and provide you with its features.")
+        Text(.init("These permissions are essential for the app to function effectively and provide you with its features."))
             .fontWeight(.regular)
             .multilineTextAlignment(.leading)
             .foregroundStyle(Color.textDark)
@@ -68,9 +68,10 @@ struct WelcomeView: View {
     }
     
     private var axiesText: some View {
-        Text("Please select how many axies do you want to track")
+        Text(.init("Please select how many axies do you want to track"))
             .fontWeight(.bold)
             .multilineTextAlignment(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
             .foregroundStyle(Color.textDark)
             .font(.roboto300, size: 18)
     }
@@ -84,7 +85,7 @@ struct WelcomeView: View {
                 navigationManager.appState = .allowPermissions
             }
         } label: {
-            Text("Ok, got it")
+            Text(.init("Ok, got it"))
                 .padding(.horizontal)
         }
         .buttonStyle(.mainBlueButton)
